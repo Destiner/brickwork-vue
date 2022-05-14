5cd32aa325cc2f8a8785e0810fae6832c503e3c0
Timur Badretdinov <destinerx@gmail.com>
2022-05-15 03:32:50 +0400

    Initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Button from './Button.vue';
+import Checkbox from './Checkbox.vue';
+import Input from './Input.vue';
+import RadioGroup from './RadioGroup.vue';
+
+export { Button, Checkbox, Input, RadioGroup };
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { Button, Checkbox, Input, RadioGroup } from './components';
+
+export { Button, Checkbox, Input, RadioGroup };
