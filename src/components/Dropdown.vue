78e1786f792c22b54a6efdfb7e895c503373c834
Timur Badretdinov <destinerx@gmail.com>
2022-07-11 00:33:45 +0400

    Add missing Dropdown implementation
    lol

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,1 +1,159 @@
-<template></template>
+<template>
+  <div class="container">
+    <Menu
+      as="div"
+      class="menu"
+    >
+      <div>
+        <MenuButton class="trigger-button">
+          <slot name="trigger" />
+        </MenuButton>
+      </div>
+
+      <transition name="show">
+        <MenuItems class="items">
+          <MenuItem
+            v-for="option in options"
+            :key="option.value"
+            v-slot="{ active }"
+          >
+            <button
+              class="item"
+              :class="{ active }"
+              @click="handleClick(option.value)"
+            >
+              {{ option.label }}
+            </button>
+          </MenuItem>
+        </MenuItems>
+      </transition>
+    </Menu>
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
+import { PropType } from 'vue';
+
+defineProps({
+  options: {
+    type: Array as PropType<Option[]>,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['select']);
+
+function handleClick(value: string): void {
+  emit('select', value);
+}
+</script>
+
+<script lang="ts">
+interface Option {
+  value: string;
+  label: string;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { Option };
+</script>
+
+<style>
+.container {
+  position: fixed;
+  text-align: right;
+}
+
+.menu {
+  display: inline-block;
+  position: relative;
+  text-align: left;
+}
+
+.trigger-button {
+  all: unset;
+  display: inline-flex;
+  flex: var(--flex-button, initial);
+  align-items: center;
+  justify-content: center;
+  padding: var(--padding-button, 10px 15px);
+  border: var(--border-button, 1px solid transparent);
+  border-radius: var(--border-radius-button, 4px);
+  background: var(--background-button, white);
+  box-shadow: var(--box-shadow-button, #00000024 0 2px 10px);
+  color: var(--color-button, #540502);
+  font-size: var(--font-size-button, 13px);
+  font-weight: var(--font-weight-button, 400);
+  line-height: 1;
+  gap: 5px;
+}
+
+.trigger-button:hover {
+  background: var(--background-button-hover, #eee);
+  box-shadow: var(--box-shadow-button-hover, #00000024 0 2px 10px);
+}
+
+.trigger-button:focus {
+  border: var(--border-button-focus, 1px solid #290402);
+}
+
+.show-enter-active,
+.show-leave-active {
+  transition: opacity 0.1s ease-in;
+}
+
+.show-enter-from,
+.show-leave-to {
+  opacity: 0;
+}
+
+.items {
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  flex-direction: column;
+  max-height: 240px;
+  margin-top: var(--gap, 4px);
+  padding: var(--padding-list, 4px);
+  overflow: auto;
+  border: var(--border-list, none);
+  border-radius: var(--border-radius-list, 6px);
+  background: var(--background-list, white);
+  box-shadow: var(
+    --box-shadow-list,
+    0 10px 15px -3px rgb(0 0 0 / 10%),
+    0 4px 6px -4px rgb(0 0 0 / 10%)
+  );
+}
+
+.items:focus {
+  outline: none;
+}
+
+.item {
+  display: flex;
+  position: relative;
+  align-items: center;
+  padding: var(--padding-item, 0 12px);
+  border: none;
+  gap: var(--gap-item, 6px);
+  border-radius: var(--border-radius-item, 4px);
+  background: none;
+  color: var(--color-item, #540502);
+  font-size: var(--font-size-item, 12px);
+  font-weight: var(--font-weight-item, 400);
+  line-height: 25px;
+  text-align: left;
+  cursor: default;
+  user-select: none;
+}
+
+.item.active {
+  background: var(--background-item-active, #540502);
+  color: var(--color-item-active, white);
+}
+</style>
