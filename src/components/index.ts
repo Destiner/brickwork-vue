9c2217a0bbf22e8c0810d969ebe204db885822f7
Timur Badretdinov <destinerx@gmail.com>
2022-07-11 00:29:02 +0400

    Export Dropdown

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import Button from './Button.vue';
 import Checkbox from './Checkbox.vue';
 import Dialog from './Dialog.vue';
+import Dropdown from './Dropdown.vue';
 import Input from './Input.vue';
 import RadioGroup from './RadioGroup.vue';
 import Select from './Select.vue';
 
-export { Button, Checkbox, Dialog, Input, RadioGroup, Select };
+export { Button, Checkbox, Dialog, Dropdown, Input, RadioGroup, Select };
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import {
   Button,
   Checkbox,
   Dialog,
+  Dropdown,
   Input,
   RadioGroup,
   Select,
 } from './components/index.js';
 
-export { Button, Checkbox, Dialog, Input, RadioGroup, Select };
+export { Button, Checkbox, Dialog, Dropdown, Input, RadioGroup, Select };
